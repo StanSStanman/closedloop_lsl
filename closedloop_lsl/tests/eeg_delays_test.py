0008c32400ebde6c3ac8d09576dea33d8293a6f7
stansstanman <ruggero.basanisi@gmail.com>
2025-03-21 12:17:00 +0100

    update code for testing network delays

--- closedloop_lsl/tests/eeg_delays_test.py
+++ closedloop_lsl/tests/eeg_delays_test.py
@@ -48,13 +48,26 @@ if __name__ == '__main__':
     streams_name = 'EE225-000000-000625'
     streams_type = 'EEG'
     
-    task = ClosedLoopLSL(sfreq=500.)
+    eeg_channels = ['0Z', '1Z', '2Z', '3Z', '4Z', '1L', '1R', 
+                    '1LB', '1RB', '2L', '2R', '3L', '3R', 
+                    '4L', '4R', '1LC', '1RC', '2LB', '2RB', 
+                    '1LA', '1RA', '1LD', '1RD', '2LC', '2RC', 
+                    '3LB', '3RB', '3LC', '3RC', '2LD', '2RD', 
+                    '3RD', '3LD', '9Z', '8Z', '7Z', '6Z', 'EOG',
+                    '10L', '10R', '9L', '9R', '8L', '8R', 
+                    '7L', '7R', '6L', '6R', '5L', '5R', 
+                    '4LD', '4RD', '5LC', '5RC', '5LB', '5RB', 
+                    '3LA', '3RA', '2LA', '2RA', '4LC', '4RC', 
+                    '4LB', '4RB', 'BIP1', 'BIP2', 'BIP3',
+                    'TRIG1', 'TRIG2']
+    
+    task = ClosedLoopLSL(sfreq=500., ch_names=eeg_channels,)
     task.search_stream(sname=streams_name, stype=streams_type)
     task.open_stream(bufsize=5.)
     task.connect_stream()
     # Add channels' names here
     task.apply_filter(low_freq=.5, high_freq=4.)
-    task.set_reference_channels([31, 32]) # Reference channels' names
+    task.set_reference_channels(['3LD', '3RD']) # Reference channels' names
     task.start_acquisition(interval=0.001)
     
     # Initialize stimulator
